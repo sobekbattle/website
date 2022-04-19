d148d3d987f30abe3341af1634ebd6a7215f63c4
sobekbattle <100533770+sobekbattle@users.noreply.github.com>
2022-04-18 19:32:21 -0700

    Adding button

--- components/nav.js
+++ components/nav.js
@@ -16,6 +16,7 @@ const Nav = () => (
       <li className="flex px-2 py-1">
         <Link href="/">
           <a className="text-blue-500 no-underline text-sm">Home</a>
+          <button type="button" className="text-gray-500 group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" aria-expanded="false"/>
         </Link>
       </li>
       {links.map(({ key, href, label }) => (
