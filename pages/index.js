1d62ccc74f9fa391b4158a311a216ce06ade6b93
sobekbattle <100533770+sobekbattle@users.noreply.github.com>
2022-04-18 21:26:47 -0700

    Adjusting layout

--- pages/index.js
+++ pages/index.js
@@ -14,17 +14,16 @@ const Home = () => (
     
     <div className='flex min-h-screen justify-center items-center'>
     <div className='max-w-lg mx-auto px-4 sm:px-6 lg:px-8" rounded overflow-hidden shadow-lg my-2'>
-    <div className='font-bold text-xl mb-2'>Next + Tailwind ❤️</div>
+    <div className='font-bold text-xl justify-center mb-2'>Sobek Battle</div>
         <img
           className='w-full'
           src="/sobek.png"
           alt='Sunset in the mountains'
         />
         <div className='px-6 py-4'>
-          <div className='font-bold text-xl mb-2'>Next + Tailwind ❤️</div>
+          <div className='font-bold text-xl mb-2'>Full Contact Crypto</div>
           <p className='text-grey-darker text-base'>
-            Next and Tailwind CSS are a match made in heaven, check out this
-            article on how you can combine these two for your next app.
+          NFT based Player vs Player competition for crypto currency rewards.
           </p>
         </div>
     </div>
