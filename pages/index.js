339e15996088d4d5051f50b4af406c7a4bab9635
sobekbattle <100533770+sobekbattle@users.noreply.github.com>
2022-04-18 18:07:01 -0700

    Adding Nav bar

--- pages/index.js
+++ pages/index.js
@@ -4,72 +4,70 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    
-<div className="py-12 bg-white">
-  <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    <div className="lg:text-center">
-      <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Transactions</h2>
-      <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">A better way to send money</p>
-      <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.</p>
+    <header>
+    {/* Navbar */}
+    <nav className="navbar navbar-expand-lg shadow-md py-2 bg-white relative flex items-center w-full justify-between">
+      <div className="px-6 w-full flex flex-wrap items-center justify-between">
+        <div className="flex items-center">
+          <button
+            className="navbar-toggler border-0 py-3 lg:hidden leading-none text-xl bg-transparent text-gray-600 hover:text-gray-700 focus:text-gray-700 transition-shadow duration-150 ease-in-out mr-2"
+            type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContentY"
+            aria-controls="navbarSupportedContentY" aria-expanded="false" aria-label="Toggle navigation">
+            <svg aria-hidden="true" focusable="false" data-prefix="fas" className="w-5" role="img"
+              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path fill="currentColor"
+                d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
+              </path>
+            </svg>
+          </button>
+        </div>
+        <div className="navbar-collapse collapse grow items-center" id="navbarSupportedContentY">
+          <ul className="navbar-nav mr-auto lg:flex lg:flex-row">
+            <li className="nav-item">
+              <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
+                href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
+                href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">Features</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
+                href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">Pricing</a>
+            </li>
+            <li className="nav-item mb-2 lg:mb-0">
+              <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
+                href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">About</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    {/* Navbar */}
+  
+    {/* Background image */}
+    <div className="relative overflow-hidden bg-no-repeat bg-cover" style="
+      background-position: 50%;
+      background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/146.webp');
+      height: 350px;
+    ">
+      <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
+        style="background-color: rgba(0, 0, 0, 0.75)">
+        <div className="flex justify-center items-center h-full">
+          <div className="text-center text-white px-6 md:px-12">
+            <h1 className="text-5xl font-bold mt-0 mb-6">Heading</h1>
+            <h3 className="text-3xl font-bold mb-8">Subeading</h3>
+            <button type="button"
+              className="inline-block px-6 py-2.5 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+              data-mdb-ripple="true" data-mdb-ripple-color="light">
+              Get started
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-
-    <div className="mt-10">
-      <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-        <div className="relative">
-          <dt>
-            <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-              {/* Heroicon name: outline/globe-alt */}
-              <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
-              </svg>
-            </div>
-            <p className="ml-16 text-lg leading-6 font-medium text-gray-900">Competitive exchange rates</p>
-          </dt>
-          <dd className="mt-2 ml-16 text-base text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
-        </div>
-
-        <div className="relative">
-          <dt>
-            <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-              {/* Heroicon name: outline/scale */}
-              <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
-              </svg>
-            </div>
-            <p className="ml-16 text-lg leading-6 font-medium text-gray-900">No hidden fees</p>
-          </dt>
-          <dd className="mt-2 ml-16 text-base text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
-        </div>
-
-        <div className="relative">
-          <dt>
-            <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-              {/* Heroicon name: outline/lightning-bolt */}
-              <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
-              </svg>
-            </div>
-            <p className="ml-16 text-lg leading-6 font-medium text-gray-900">Transfers are instant</p>
-          </dt>
-          <dd className="mt-2 ml-16 text-base text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
-        </div>
-
-        <div className="relative">
-          <dt>
-            <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-              {/* Heroicon name: outline/annotation */}
-              <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-              </svg>
-            </div>
-            <p className="ml-16 text-lg leading-6 font-medium text-gray-900">Mobile notifications</p>
-          </dt>
-          <dd className="mt-2 ml-16 text-base text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
-        </div>
-      </dl>
-    </div>
-  </div>
-</div>
+    {/* Background image */}
+  </header>
 
   );
 }
