31f8146c38a931271efc9bd32489d09ecda4d0d8
sobekbattle <100533770+sobekbattle@users.noreply.github.com>
2022-04-18 19:53:37 -0700

    Trying something

--- pages/index.js
+++ pages/index.js
@@ -11,51 +11,21 @@ const Home = () => (
 
     <Nav />
 
-    <div className="w-full text-gray-900">
-      <h1 className="m-0 w-full pt-20 leading-tight text-5xl text-center font-bold">
-        Welcome to Next.js!
-      </h1>
-      <p className="text-center my-4 text-m">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className="max-w-4xl mx-auto pt-20 py-auto pb-8 flex flex-row justify-around">
-        <a
-          href="https://nextjs.org/docs"
-          className="pt-4 px-5 pb-6 w-64 text-left no-underline text-gray-800 border border-gray-400 hover:border-blue-500"
-        >
-          <h3 className="m-0 text-blue-500 text-lg font-bold">
-            Documentation &rarr;
-          </h3>
-          <p className="m-0 pt-3 py-0 pb-0 text-sm text-gray-900">
-            Learn more about Next.js in the documentation.
+    <div className='max-w-xs rounded overflow-hidden shadow-lg my-2'>
+        <img
+          className='w-full'
+          src='https://tailwindcss.com/img/card-top.jpg'
+          alt='Sunset in the mountains'
+        />
+        <div className='px-6 py-4'>
+          <div className='font-bold text-xl mb-2'>Next + Tailwind ❤️</div>
+          <p className='text-grey-darker text-base'>
+            Next and Tailwind CSS are a match made in heaven, check out this
+            article on how you can combine these two for your next app.
           </p>
-        </a>
-        <a
-          href="https://nextjs.org/learn"
-          className="pt-4 px-5 pb-6 w-64 text-left no-underline text-gray-800 border border-gray-400 hover:border-blue-500"
-        >
-          <h3 className="m-0 text-blue-500 text-lg font-bold">
-            Next.js Learn &rarr;
-          </h3>
-          <p className="m-0 pt-3 py-0 pb-0 text-sm text-gray-900">
-            Learn about Next.js by following an interactive tutorial!
-          </p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="pt-4 px-5 pb-6 w-64 text-left no-underline text-gray-800 border border-gray-400 hover:border-blue-500"
-        >
-          <h3 className="m-0 text-blue-500 text-lg font-bold">
-            Examples &rarr;
-          </h3>
-          <p className="m-0 pt-3 py-0 pb-0 text-sm text-gray-900">
-            Find other example boilerplates on the Next.js GitHub.
-          </p>
-        </a>
+        </div>
       </div>
     </div>
-  </div>
 );
 
 export default Home;
