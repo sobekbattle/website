6d2d6f3b1db3c6e86b9c69ad4068bc3fbb4cdc9c
sobekbattle <100533770+sobekbattle@users.noreply.github.com>
2022-04-18 19:21:40 -0700

    Trying new set up

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
+import React from "react";
+import App from "next/app";
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default class TailwindApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
 }
-
-export default MyApp
