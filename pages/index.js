68338cb4c8332b95d3c1b26bca445e817ece13d8
sobekbattle <100533770+sobekbattle@users.noreply.github.com>
2022-04-18 20:51:59 -0700

    Trying somethign else

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const Home = () => (
 
     <Nav />
     
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div className='flex min-h-screen justify-center items-center'>
     <div className='max-w-xs rounded overflow-hidden shadow-lg my-2'>
         <img
           className='w-full'
