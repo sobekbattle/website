aa91bf2e944e86bad862b17e4881042631c82f91
sobekbattle <100533770+sobekbattle@users.noreply.github.com>
2022-05-23 23:13:19 -0700

    More fixes grid cols

--- pages/lore.js
+++ pages/lore.js
@@ -12,7 +12,7 @@ const Lore = () => (
 
     <Nav />
     <div className='font-bold text-4xl text-center justify-center mb-2'>Lore</div>
-    <div className='flex grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6'>
+    <div className='flex grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
     <div className='max-w-lg mx-auto px-4 sm:px-6 lg:px-8" rounded overflow-hidden shadow-lg my-2'>
     
         <div className='px-6 py-4'>
