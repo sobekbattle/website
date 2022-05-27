8e7b63b5ce1e0094770173ebd553f15a2b0136d0
sobekbattle <100533770+sobekbattle@users.noreply.github.com>
2022-05-26 18:35:19 -0700

    Adding perm discord link

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <>
       <footer className={styles.footer}>
         <a href="https://twitter.com/SobekBattle"><img src="/twitter.svg" alt="Twitter" className={styles.logo} /></a>
-        <a href="https://discord.gg/Www7MK43UE"><img src="/discord.svg" alt="Discord" className={styles.logo} /></a>
+        <a href="https://discord.gg/v4PwaamGd3"><img src="/discord.svg" alt="Discord" className={styles.logo} /></a>
       </footer>
     </>
   )
